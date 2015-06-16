b4de1306c2819fad505c21ddf7ba64973b6de354
keithkade <keithkade@aol.com>
2015-06-15 23:30:50 -0400

    removing retweets now works

--- OC-Only/main.js
+++ OC-Only/main.js
@@ -2,16 +2,24 @@
 
 //returns the nth parent of an element
 function nthParent(elem, n){
-    for (var i=0; i<=n; i++){
+    for (var i=0; i<n; i++){
         elem = elem.parentElement;
     } 
     return elem;
 }
 
-//checks every two seconds for new shared posts
+//check every two seconds for new shared posts and retweets
 chrome.storage.local.get('filterShares', function (result) {
     if (result.filterShares){
+        removeShares();
         setInterval(removeShares, 2000);
+    }
+});
+
+chrome.storage.local.get('filterRetweets', function (result) {
+    if (result.filterRetweets){
+        removeRetweets();
+        setInterval(removeRetweets, 2000);
     }
 });
 
@@ -21,7 +29,19 @@ function removeShares(){
     var shares = document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     if (shares.snapshotLength > 0) {
         for (var i=0; i<shares.snapshotLength; i++){
-            var post = nthParent(shares.snapshotItem(i), 11);
+            var post = nthParent(shares.snapshotItem(i), 12);
+            post.parentNode.removeChild(post);
+        }
+    }
+}
+
+//actually remove the retweets html nodes
+function removeRetweets(){
+    var path = "//span[@class='js-retweet-text']";
+    var shares = document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    if (shares.snapshotLength > 0) {
+        for (var i=0; i<shares.snapshotLength; i++){
+            var post = nthParent(shares.snapshotItem(i), 4);
             post.parentNode.removeChild(post);
         }
     }
